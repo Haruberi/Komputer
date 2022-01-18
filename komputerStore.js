922badff49aa24c2d060a50b9400274517684559
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-18 08:57:17 +0100

    add getElementById values

---
+++ komputerStore.js
@@ -0,0 +1,13 @@
+//The buttons from komputerStore.html
+const getALoanBtnElement = document.getElementById("getALoanBtn");
+const getBankBtnElement = document.getElementById("bankBtn");
+const getWorkBtnElement = document.getElementById("workBtn");
+//Get variables
+const getBankBalanceElement = document.getElementById("bankBalance");
+const getOutstandingLoanElement = document.getElementById("outstandingLoanValue");
+const getPayBalanceElement = document.getElementById("payBalance");
+const getFeaturesElement = document.getElementById("featuresTitle");
+const getComputerSpecs = document.getElementById("computerSpecs");
+//Select
+const getComputersSelectElement = document.getElementById("computers");
+
