6810c095bf8c5b6181044deec77773d73f725143
Haruberi <anna.h.hallberg.92@gmail.com>
2022-01-18 09:34:48 +0100

    add fetch

--- komputerStore.js
+++ komputerStore.js
@@ -23,4 +23,27 @@ let computers = [];
 
 ////////////////////////////////BANK
 ////////////////////////////////WORK
+
+
 ////////////////////////////////LAPTOP
+fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
+    .then(response => response.json())
+    .then(data => computers = data)
+    .then(computers => addComputersToMenu(computers));
+
+    const addComputersToMenu = (computers) => {
+        computers.forEach(x => addComputersToMenu(x));
+        getComputerSpecs.innerText = computers[0].specs;
+    }
+
+    const addComputerToMenu = (computer) => {
+        const getComputerElement = document.createElement("option");
+        getComputerElement.value = computer.id;
+        getComputerElement.appendChild(document.createTextNode(computer.title));
+        getComputersSelectElement.appendChild(computerElement);
+    }
+
+    const handleComputerMenuChange = e => {
+        const getSelectedComputer = computers[e.target.selectedIndex];
+        getComputerSpecs.innerText = getSelectedComputer.specs;
+    }
